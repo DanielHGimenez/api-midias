4ad3543b29656b2048802eab3e47ab124f822fc1
DanielHGimenez <danielhgimenez027@gmail.com>
2020-09-09 14:52:49 -0300

    Setup do projeto

---
+++ src/main/java/dev/dhg/apimidias/ApiMidiasApplication.java
@@ -0,0 +1,13 @@
+package dev.dhg.apimidias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMidiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMidiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dhg/apimidias/ApiMidiasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dhg.apimidias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMidiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
