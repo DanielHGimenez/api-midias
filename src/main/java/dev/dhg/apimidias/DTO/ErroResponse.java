d7df80e5b143befb7dec5b1fd9819e3102bccdc1
DanielHGimenez <danielhgimenez027@gmail.com>
2020-09-12 11:44:45 -0300

    adicionando construtores

--- src/main/java/dev/dhg/apimidias/DTO/ErroResponse.java
+++ src/main/java/dev/dhg/apimidias/DTO/ErroResponse.java
@@ -1,11 +1,14 @@
 package dev.dhg.apimidias.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErroResponse<T> {
 
     private T mensagem;
