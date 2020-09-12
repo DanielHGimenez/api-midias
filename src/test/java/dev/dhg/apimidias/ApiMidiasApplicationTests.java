be4b1c6a63082d614ba86534eb8b5939904707ab
DanielHGimenez <danielhgimenez027@gmail.com>
2020-09-12 11:43:39 -0300

    implementação de teste de carregamento de contexto

--- src/test/java/dev/dhg/apimidias/ApiMidiasApplicationTests.java
+++ src/test/java/dev/dhg/apimidias/ApiMidiasApplicationTests.java
@@ -1,13 +1,27 @@
 package dev.dhg.apimidias;
 
+import dev.dhg.apimidias.controller.MediaController;
+import dev.dhg.apimidias.service.MediaService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiMidiasApplicationTests {
+
+	@Autowired
+	private MediaController mediaController;
+
+	@Autowired
+	private MediaService mediaService;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(mediaController).isNotNull();
+		Assertions.assertThat(mediaService).isNotNull();
 	}
 
 }
